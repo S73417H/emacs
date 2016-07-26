b728d764bea6afcdca4dfe72c38ac393815eab2f
Michael Fazio <michaelfazio@me.com>
2016-07-26 21:49:41 +0800

    Added installation of monokai theme

--- startup.el
+++ startup.el
@@ -5,9 +5,14 @@
 (if (not (package-installed-p 'sr-speedbar))
     (package-install 'sr-speedbar))
 
+(if (not (package-installed-p 'monokai-theme))
+    (package-install 'monokai-theme))
+
 (add-hook 'emacs-startup-hook
           (lambda ()
             (sr-speedbar-open)
             ))
+
+(load-theme 'monokai)
 
 ;;; startup.el ends here
